8c1e1aa3e8ffa236a65b3c1b06cf381cd26383e7
aphitep <aumaphitep@gmail.com>
2026-03-31 11:27:34 +0700

    add todolist colors

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,14 +1,11 @@
 import "./App.css";
-
-const todos = ["Eat breakfast", "Go to work", "Go to sleep"];
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
-    <div className="container shadow-lg rounded-xl p-5">
+    <div className="container shadow-lg rounded-xl p-5 w-300">
       <p className="text-center text-xl font-bold">Todo List</p>
-      {todos.map((todo, index) => {
-        return <p key={index}>{todo}</p>;
-      })}
+      <TodoList />
     </div>
   );
 }
---
+++ todolist/src/components/TodoList.tsx
@@ -0,0 +1,14 @@
+const todos = ["Eat breakfast", "Go to work", "Go to sleep"];
+export default function TodoList() {
+  return (
+    <ul className="list bg-[#FFFBF1] rounded-sm shadow-md mt-2">
+      {todos.map((todo, index) => {
+        return (
+          <li className="list-row" key={index}>
+            <div>{todo}</div>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
