89c9a7b29b40418d844ff4901203556f85d39c2c
aphitep <aumaphitep@gmail.com>
2026-03-31 10:56:15 +0700

    add daisyUI

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
 
+const todos = ["Eat breakfast", "Go to work", "Go to sleep"];
+
 function App() {
-  return <>Hello World</>;
+  return (
+    <div className="container shadow-lg rounded-xl p-5">
+      <p className="text-center text-xl font-bold">Todo List</p>
+      {todos.map((todo, index) => {
+        return <p key={index}>{todo}</p>;
+      })}
+    </div>
+  );
 }
 
 export default App;
